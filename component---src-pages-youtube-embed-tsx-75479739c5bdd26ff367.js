e697890e2845d75f6f10f304a8319576a538d900
Pascal Friedrichsen <pascal.friedrichsen@stud.hs-flensburg.de>
2023-12-19 01:54:09 +0100

    Updates

---
+++ component---src-pages-youtube-embed-tsx-75479739c5bdd26ff367.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkbartresor=self.webpackChunkbartresor||[]).push([[680],{8378:function(e,r,t){t.r(r);var a=t(7294);r.default=e=>{let{embedId:r}=e;return a.createElement("div",{className:"video-responsive"},a.createElement("iframe",{width:"853",height:"480",src:`https://www.youtube.com/embed/${r}`,frameBorder:"0",allow:"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture",allowFullScreen:!0,title:"Embedded youtube"}))}}}]);
+//# sourceMappingURL=component---src-pages-youtube-embed-tsx-75479739c5bdd26ff367.js.map
